a6e7704e5c037a715b917dbbf4a7fcdcbe3fc363
alicewonders <32062286+alicewonders@users.noreply.github.com>
2018-06-10 19:33:10 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import controller.SlidePuzzleController;
+
+public class Main {
+    public static void main(String[] args) {
+        SlidePuzzleController controller = new SlidePuzzleController();
+        controller.start();
+    }
+}
